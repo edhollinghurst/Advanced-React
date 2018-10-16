536f18085400be796f7927444e2835f66ea29bd3
Ed Hollinghurst <hello@edhollinghurst.com>
2018-10-16 17:21:53 +0100

    End of lesson 16

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import styled from 'styled-components';
+import Item from './Item';
 
 const ALL_ITEMS_QUERY = gql`
   query ALL_ITEMS_QUERY {
@@ -24,7 +25,7 @@ const ItemsList = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: 60px;
-  max-width: ${props => props.theme.maxWidth};
+  max-width: ${(props) => props.theme.maxWidth};
   margin: 0 auto;
 `;
 
@@ -32,17 +33,21 @@ export default class Items extends Component {
   render() {
     return (
       <Center>
-        <p>Items!</p>
         <Query query={ALL_ITEMS_QUERY}>
-          {({ data, error, loading }) => { // Destructure the payload.
-            if (loading) return <p>Loading....</p>
-            if (error) return <p>Error.... {error.message}</p>
-            return <ItemsList>
-              {data.items.map(item => <p>{item.title}</p>)}
-            </ItemsList>
+          {({ data, error, loading }) => {
+            // Destructure the payload.
+            if (loading) return <p>Loading....</p>;
+            if (error) return <p>Error.... {error.message}</p>;
+            return (
+              <ItemsList>
+                {data.items.map((item) => (
+                  <Item key={item.id} item={item} />
+                ))}
+              </ItemsList>
+            );
           }}
-       </Query>
+        </Query>
       </Center>
-    )
+    );
   }
 }
