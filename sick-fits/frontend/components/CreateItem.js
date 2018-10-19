7bb84e1b4e6d514ff3d0e5b6a9c8ea11c3fad1bc
Ed Hollinghurst <hello@edhollinghurst.com>
2018-10-19 16:23:16 +0100

    End of lesson 18

--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -44,6 +44,26 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async (e) => {
+    const files = e.target.files;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickfits');
+    // Using Fetch API.
+    const res = await fetch(
+      'https://api.cloudinary.com/v1_1/ed-hollinghurst/image/upload',
+      {
+        method: 'POST',
+        body: data,
+      }
+    );
+    const file = await res.json();
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -61,6 +81,25 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  // value={this.state.image}
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && (
+                  <img
+                    src={this.state.image}
+                    alt="Upload preview"
+                    width="200"
+                  />
+                )}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
