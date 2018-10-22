4ea27b2cc1fda24730be9d53b63f24b3ddf42994
Ed Hollinghurst <hello@edhollinghurst.com>
2018-10-22 22:41:06 +0100

    End of lesson 20

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -16,20 +16,29 @@ const Mutations = {
     return item;
   },
   updateItem(parent, args, ctx, info) {
-    // first take a copy of the update
+    // first take a copy of the updates
     const updates = { ...args };
     // remove the ID from the updates
     delete updates.id;
     // run the update method
-    // ctx - context in the request
-    // ctx.db - how we expose the prisma db
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id,
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
       },
-      info,
-    });
+      info
+    );
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
   },
 };
 
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -10,13 +10,4 @@ const Query = {
   // }
 };
 
-const Query = {
-  items: forwardTo('db') // Forward the query from Yoga to Prisma
-  // async items(parent, args, ctx, info) {
-  //   console.log('Getting Items!!');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
-};
-
 module.exports = Query;
