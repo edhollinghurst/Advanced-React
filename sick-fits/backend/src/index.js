92bffcdca017f1459726f045c2aae9f170fc8185
Ed Hollinghurst <hello@edhollinghurst.com>
2018-10-26 15:45:15 +0100

    End of lesson 24

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,11 +1,12 @@
+const cookieParser = require('cookie-parser');
+
 require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express middlware to handle cookies (JWT)
-
+server.express.use(cookieParser());
 // TODO Use express middlware to populate current user
 
 server.start(
@@ -15,7 +16,7 @@ server.start(
       origin: process.env.FRONTEND_URL,
     },
   },
-  deets => {
-    console.log(`Server is now running on port http:/localhost:${deets.port}`);
+  (deets) => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
   }
 );
