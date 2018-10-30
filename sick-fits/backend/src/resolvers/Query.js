65675d94fd6d6b63fe48a12460e2f164b6bd57d8
Ed Hollinghurst <hello@edhollinghurst.com>
2018-10-30 17:11:08 +0000

    End of lesson 26

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,13 +2,21 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'), // Forward the query from Yoga to Prisma (no extra logic needed)
-  item: forwardTo('db'), // Forward the query from Yoga to Prisma (no extra logic needed)
-  itemsConnection: forwardTo('db'), // Forward the query from Yoga to Prisma (no extra logic needed)
-  // async items(parent, args, ctx, info) {
-  //   console.log('Getting Items!!');
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // }
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+  // me() is a ES6 shorthand method syntax for me: function()
+  me(parent, args, ctx, info) {
+    // check if there is a current user ID
+    if (!ctx.request.userId) {
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId },
+      },
+      info
+    ); // `info` is the query that's coming from the client side.
+  },
 };
 
 module.exports = Query;
