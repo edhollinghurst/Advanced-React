60e2ec117957235d31416bf4b2d784288850553c
Ed Hollinghurst <hello@edhollinghurst.com>
2018-12-09 13:52:26 +0000

    End of lesson 39

--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -1,17 +1,41 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
 
 function createClient({ headers }) {
   return new ApolloClient({
     uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
+    request: (operation) => {
       operation.setContext({
         fetchOptions: {
           credentials: 'include', // Always include cookies with any requests.
         },
         headers,
       });
+    },
+    // Local data
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            // 1. Read the Cart Open value from the cache
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY,
+            });
+            // 2. Write the Cart state to the opposite.
+            const data = {
+              data: { cartOpen: !cartOpen },
+            };
+            cache.writeData(data);
+            return data;
+          },
+          // the _ first param in toggleCart() - even Wes not sure what it's for!
+        },
+      },
+      defaults: {
+        cartOpen: false,
+      },
     },
   });
 }
